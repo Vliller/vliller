a6fa2b0fb6d88d52e21797f903ac142e1efecc21
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2016-09-30 13:19:32 +0200

    Implements rate app logic

--- www/app/app.module.js
+++ www/app/app.module.js
@@ -12,5 +12,8 @@
         'vliller.home',
         'vliller.sidemenu'
     ])
-    .constant('VLILLE_PROXY_URL', 'http://dev.alexandrebonhomme.fr/vlille/web');
+    .constant('VLILLE_PROXY_URL', 'http://dev.alexandrebonhomme.fr/vlille/web')
+
+    .constant('ANDROID_APP_ID', 'com.alexetmanon.vliller')
+    .constant('IOS_APP_ID', '');
 }());
--- www/app/components/sidemenu/sidemenu.controller.js
+++ www/app/components/sidemenu/sidemenu.controller.js
@@ -1,11 +1,18 @@
 (function () {
     angular
     .module('vliller.sidemenu')
-    .controller('SidemenuController', ['$scope', '$rootScope', function ($scope, $rootScope) {
+    .controller('SidemenuController', ['ANDROID_APP_ID', 'IOS_APP_ID', function (ANDROID_APP_ID, IOS_APP_ID) {
         var vm = this;
 
-
-
+        vm.rateApp = function () {
+            if (ionic.Platform.isAndroid()) {
+                window.open('market://details?id=' + ANDROID_APP_ID);
+            } else if (ionic.Platform.isIOS()) {
+                window.open('itms-apps://itunes.apple.com/fr/app/vliller/id' + IOS_APP_ID + '?mt=8');
+            } else {
+                console.error('Unknow platform?!');
+            }
+        };
 
     }]);
 }());
