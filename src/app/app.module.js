8ab5839123ac22071508d9975b7ba5fb9fffabdf
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2016-10-05 16:25:46 +0200

    Removes useless instabug constants

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -19,8 +19,5 @@
     .constant('ANDROID_APP_ID', 'com.alexetmanon.vliller')
     .constant('IOS_APP_ID', '')
 
-    .constant('INSTABUG_ANDROID_TOKEN', '64e51d98195423eb197dc88178bf7557')
-    .constant('INSTABUG_IOS_TOKEN', '094f2566c433fd994f964d6fcb62288d')
-
     .constant('VLILLER_SITE_URL', 'http://vliller.alexetmanon.com');
 }());
