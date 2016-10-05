76737c98824100a9b5b611938cb1cff8634bae12
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2016-10-05 17:47:25 +0200

    Update iTunes app id

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -17,7 +17,7 @@
     .constant('VLILLE_PROXY_URL', 'http://dev.alexandrebonhomme.fr/vlille/web')
 
     .constant('ANDROID_APP_ID', 'com.alexetmanon.vliller')
-    .constant('IOS_APP_ID', '')
+    .constant('IOS_APP_ID', '1161025016')
 
     .constant('GOOGLE_ANALYTICS_ID', 'UA-85251159-1')
 
