383cf8e01969f7dee4045ada06cde6fae5d3fcc3
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2016-10-10 23:28:48 +0200

    Refactor Navigation service to use plugin.google.maps.external.launchNavigation

--- src/app/shared/navigation/navigation.service.js
+++ src/app/shared/navigation/navigation.service.js
@@ -3,36 +3,17 @@
 
     angular
         .module('vliller.navigation', [])
-        .factory('Navigation', ['$q', '$log', function ($q, $log) {
-            var navigationApp;
-
-            // Defines Google Maps by default if avaible
-            document.addEventListener('deviceready', function () {
-                launchnavigator.isAppAvailable(launchnavigator.APP.GOOGLE_MAPS, function (isAvailable) {
-                    if (isAvailable) {
-                        navigationApp = launchnavigator.APP.GOOGLE_MAPS;
-                    } else {
-                        $log.debug("Google Maps not available - falling back to user selection");
-                        navigationApp = launchnavigator.APP.USER_SELECT;
-                    }
-                });
-            });
-
+        .factory('Navigation', ['$q', function ($q) {
             return {
                 navigate: function (from, to) {
                     var defer = $q.defer();
 
                     document.addEventListener('deviceready', function () {
                         // navigate to the station from current position
-                        launchnavigator.navigate([
-                            to.latitude,
-                            to.longitude
-                        ], {
-                            app: navigationApp || launchnavigator.APP.USER_SELECT,
-                            start: [
-                                from.latitude,
-                                from.longitude
-                            ]
+                        plugin.google.maps.external.launchNavigation({
+                            from: new plugin.google.maps.LatLng(from.latitude, from.longitude),
+                            to: new plugin.google.maps.LatLng(to.latitude, to.longitude),
+                            travelMode: 'walking'
                         });
                     }, false);
 
