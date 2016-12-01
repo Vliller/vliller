10a4941c0f7df757f1c9f52b2d0798391c712b38
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2016-12-01 20:37:38 +0100

    Adds vlille service

---
+++ src/components/vlilles/vlilles.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+import { VlilleStation } from './vlille-station';
+
+@Injectable()
+export class Vlilles {
+    get(): VlilleStation {
+        // TODO
+        return null;
+    }
+
+    getAll(): VlilleStation[] {
+        // TODO
+        return [];
+    }
+}
