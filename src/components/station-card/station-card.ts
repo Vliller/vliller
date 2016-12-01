d882f25b23271303cfee6648650a94884b1a2abf
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2016-12-01 15:03:12 +0100

    Adds stations card components du display station details

---
+++ src/components/station-card/station-card.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'station-card',
+    templateUrl: './station-card.html'
+})
+
+export class StationCard {
+    constructor() {
+
+    }
+}
