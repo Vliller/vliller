599f9e5cc36bd74c6d7192e7c29e266c22f9b973
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2016-12-04 20:44:12 +0100

    Implements card station WIP

--- src/components/station-card/station-card.ts
+++ src/components/station-card/station-card.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { VlilleStationDetails } from '../vlille/vlille';
 
 @Component({
     selector: 'station-card',
@@ -6,5 +9,79 @@ import { Component } from '@angular/core';
 })
 
 export class StationCard {
-    constructor() {}
+    public stationDetails: VlilleStationDetails;
+
+    @Input() station: Observable<VlilleStationDetails>;
+
+    constructor() {
+        this.station.subscribe((station: VlilleStationDetails) => this.stationDetails = station);
+    }
+
+    /**
+     * Compute css class according to the given number.
+     * 0        : red,
+     * ]0, 5]   : orange,
+     * ]5, inf[ : green
+     *
+     * @param  Number number
+     * @return String
+     */
+    public computeColorClass (number: number) {
+        if (number === 0) {
+            return 'assertive';
+        }
+
+        if (number <= 5) {
+            return 'energized';
+        }
+
+        return 'calm';
+    };
+
+    public computeBikesIcon (number: number) {
+        if (number === 0) {
+            return 'assets/img/vliller_bike-red.png';
+        }
+
+        if (number <= 5) {
+            return 'assets/img/vliller_bike-orange.png';
+        }
+
+        return 'assets/img/vliller_bike-green.png';
+    };
+
+    public computeDocksIcon (number: number) {
+        if (number === 0) {
+            return 'assets/img/vliller_place-red.png';
+        }
+
+        if (number <= 5) {
+            return 'assets/img/vliller_place-orange.png';
+        }
+
+        return 'assets/img/vliller_place-green.png';
+    };
+
+    /**
+     * Return a string format in meters or kilometers.
+     *
+     * @return String
+     */
+    public formatedDistance () {
+        // var distanceInMeter = activeMarker.get('distance'),
+        //     distanceString = 'à ';
+
+        // // meters
+        // if (distanceInMeter < 1000) {
+        //     distanceString += Math.round(distanceInMeter) + 'm';
+        // }
+
+        // // kilometers
+        // // @see http://www.jacklmoore.com/notes/rounding-in-javascript/
+        // else {
+        //     distanceString += Number(Math.round((distanceInMeter / 1000) + 'e1') + 'e-1') + 'km';
+        // }
+
+        // return distanceString;
+    };
 }
