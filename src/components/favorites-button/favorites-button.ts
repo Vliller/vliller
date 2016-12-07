4743f592ca570cf85a80835a3b92b7be986ad4f1
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2016-12-08 00:02:07 +0100

    Implements favorites buttons

--- src/components/favorites-button/favorites-button.ts
+++ src/components/favorites-button/favorites-button.ts
@@ -1,8 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 import { VlilleStation } from '../../services/vlille/vlille';
-
-const FAVORITES_MAX_SIZE = 4;
 
 @Component({
     selector: 'favorites-button',
@@ -10,52 +9,7 @@ const FAVORITES_MAX_SIZE = 4;
 })
 
 export class FavoritesButton {
-    private favoriteStations: VlilleStation[];
+    @Input() favoriteStations: Observable<VlilleStation[]>;
 
-    constructor() {
-        // TODO: init from localstorage
-        this.favoriteStations = [];
-    }
-
-    /**
-     *
-     * @param  {VlilleStation} station
-     * @return {boolean}
-     */
-    public add(station: VlilleStation): boolean {
-        if (this.favoriteStations.length === FAVORITES_MAX_SIZE) {
-            return false;
-        }
-
-        // checks if the station already is in the array
-        for (let favoriteStation of this.favoriteStations) {
-            if (favoriteStation.id === station.id) {
-                return true;
-            }
-        }
-
-        // add station the the array
-        return !!this.favoriteStations.push(station);
-    }
-
-    /**
-     *
-     * @param  {VlilleStation} station
-     * @return {boolean}
-     */
-    public remove(station: VlilleStation): boolean {
-        if (this.favoriteStations.length === 0) {
-            return false;
-        }
-
-        // removes the station if it's in the fav array
-        for (let i = 0, len = this.favoriteStations.length; i < len; i += 1) {
-            if (this.favoriteStations[i].id === station.id) {
-                return !!this.favoriteStations.splice(i);
-            }
-        }
-
-        // add station the the array
-        return true;
-    }
+    constructor() {}
 }
