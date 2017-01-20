98ec1c72780879c75aaaebd39d7893186ee7a07d
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2017-01-20 19:26:10 +0100

    Implements location request and manage disabled location

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component, NgZone } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
+import { LocationAccuracy, Diagnostic } from 'ionic-native';
+import { AlertController } from 'ionic-angular';
 
 import { VlilleService, VlilleStationResume, VlilleStation } from '../../services/vlille/vlille';
 import { FavoritesService } from '../../services/favorites/favorites';
@@ -23,7 +25,8 @@ export class Home {
         private zone: NgZone,
         private vlilleService: VlilleService,
         private favoritesService: FavoritesService,
-        private locationService: LocationService
+        private locationService: LocationService,
+        private alertController: AlertController
     ) {
         this.stations = vlilleService.getAllStations();
 
@@ -32,7 +35,39 @@ export class Home {
         this.currentPosition = locationService.asObservable();
 
         // gets initial position
-        locationService.updateCurrentPosition();
+        locationService.requestLocation().then(() => locationService.updateCurrentPosition().then(error => {
+            if (error === 'locationDisabled') {
+                // TODO: toast
+                // aetmToastService.showError('Vous devez activer votre GPS pour utiliser cette fonctionnalité.', 'long');
+
+                return error;
+            }
+        }), error => {
+            // Android only
+            if (error && error.code !== LocationAccuracy.ERROR_USER_DISAGREED) {
+
+                // open popup asking for settings
+                return this.alertController.create({
+                    title: 'Vliller a besoin de votre position',
+                    message: "Impossible d'activer le GPS automatiquement. Voullez-vous ouvrir les préférences et l'activer la localisation \"haute précision\" manuellement ?",
+                    buttons: [{
+                        text: 'Annuler',
+                        handler: () => {
+                            // throw an error
+                            throw {
+                              code: LocationAccuracy.ERROR_USER_DISAGREED
+                            };
+                        }
+                    },
+                    {
+                        text: 'Ouvrir les paramètres',
+                        handler: () => {
+                            Diagnostic.switchToLocationSettings();
+                        }
+                    }]
+                }).present();
+            }
+        });
     }
 
     /**
--- src/services/location/location.ts
+++ src/services/location/location.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import { Platform } from 'ionic-angular';
-import { Geolocation, Geoposition } from 'ionic-native';
+import { Geolocation, Geoposition, Diagnostic, LocationAccuracy } from 'ionic-native';
 
 export interface Position extends Geoposition {}
 
@@ -12,12 +12,30 @@ export class LocationService {
 
     constructor(private platform: Platform) {}
 
-    public updateCurrentPosition() {
-        this.platform.ready().then(() =>
-            Geolocation.getCurrentPosition().then(
-                position => this.currentPositionSubject.next(position)
-            ).catch(error => console.log(error))
+    public updateCurrentPosition(): Promise<any> {
+        return this.platform.ready().then(() => Diagnostic.isLocationEnabled()
+            .then(isLocationEnabled => {
+
+                // GPS disabled
+                if (!isLocationEnabled) {
+                    return Promise.reject('locationDisabled');
+                }
+
+                // Get current location
+                return Geolocation.getCurrentPosition()
+            })
+            .then((position: Geoposition) => this.currentPositionSubject.next(position))
         );
+    }
+
+    public requestLocation(): Promise<any> {
+        return this.platform.ready().then(() => LocationAccuracy.canRequest().then(canRequest => {
+            if (!canRequest) {
+                return Promise.resolve();
+            }
+
+            return LocationAccuracy.request(LocationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY);
+        }));
     }
 
     public asObservable(): Observable<Geoposition> {
