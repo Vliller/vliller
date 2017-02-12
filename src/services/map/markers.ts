c8595c18d21532fc5d732acef1f641ad1cb6c5bb
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2017-02-12 18:07:50 +0100

    Implements a MarkersService to store map marker using a Map structure

---
+++ src/services/map/markers.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { VlilleStationResume } from '../vlille/vlille';
+
+@Injectable()
+export class MarkersService {
+    /**
+     * key: station id
+     * value: google.maps.Marker
+     */
+    private markers = new Map<string, any>();
+
+    public set(stationId: string, marker: any) {
+        this.markers.set(stationId, marker);
+    }
+
+    public remove(stationId: string) {
+        this.markers.delete(stationId);
+    }
+
+    public get(stationId: string): any {
+        return this.markers.get(stationId);
+    }
+
+    public getAll(): any {
+        return this.markers.values();
+    }
+}
