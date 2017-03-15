2ec980aec3bfeb2d30f9f8f7ffff012faf6dbb98
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2017-03-15 23:58:44 +0100

    Replace Sentry API bu Doorbell one

--- src/services/feedback-form/feedback-form.ts
+++ src/services/feedback-form/feedback-form.ts
@@ -1,22 +1,25 @@
 import { Injectable, Component } from '@angular/core';
-import { ModalController, ViewController } from 'ionic-angular';
+import { ModalController, ViewController, Platform } from 'ionic-angular';
 import { Headers, RequestOptions, Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import * as Raven from 'raven-js';
+import { Device, AppVersion } from 'ionic-native';
 
 @Injectable()
 export class FeedbackFormService {
     constructor(
         private modalCtrl: ModalController
-    ) {}
+    ) {
+
+    }
 
     public showModal() {
         this.modalCtrl.create(FeedbackFrom).present();
     }
 }
 
-const FEEDBACK_API_URL = 'http://app.getsentry.com/api/0/projects/alex-et-manon/vliller/user-feedback/';
-const FEEDBACK_API_TOKEN = 'fd7bb0c52cd246b0aad1acb7c482b6edce35f0750d7a494fa495cb606dc911f4';
+const FEEDBACK_API_URL = 'https://doorbell.io/api/applications/4561/submit';
+const FEEDBACK_API_TOKEN = 'g9xKf3v4aM29diiMXJVh2Ko9J54fEaQ6uCqysESJSf8WWaKIcXwmVBXT94rXF8Lr';
 
 @Component({
     templateUrl: 'feedback-form.html'
@@ -26,26 +29,36 @@ export class FeedbackFrom {
 
     constructor(
         private viewCtrl: ViewController,
-        private http: Http
+        private http: Http,
+        private platform: Platform
     ) {}
 
     public submit() {
-        let eventId = Raven.captureMessage('FeedbackFrom');
+        this.platform.ready()
+            .then(() => AppVersion.getVersionNumber())
+            .then(version => {
+            this.sendRequest({
+                email: this.userFeedback.email,
+                message: this.userFeedback.message,
+                properties: {
+                    version: version,
+                    device: {
+                        cordova: Device.cordova,
+                        model: Device.model,
+                        platform: Device.platform,
+                        version: Device.version,
+                        manufacturer: Device.manufacturer
+                    }
+                }
+            })
+            .catch(error => {
+                Raven.captureException(new Error(error));
 
-        this.sendRequest({
-            "event_id": eventId,
-            "email": this.userFeedback.email,
-            "comments": this.userFeedback.message,
-            "name": this.userFeedback.name
-        })
-        .catch(error => {
-            // TODO: handle error
-            console.error(error);
-
-            return Observable.throw(error);
-        })
-        .subscribe(() => {
-            this.close();
+                return Observable.throw(error);
+            })
+            .subscribe(() => {
+                this.close();
+            });
         });
     }
 
@@ -55,13 +68,12 @@ export class FeedbackFrom {
 
     private sendRequest(data: any): Observable<Response> {
         let headers = new Headers({
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + FEEDBACK_API_TOKEN
+            'Content-Type': 'application/json'
         });
         let options = new RequestOptions({
             headers: headers
         });
 
-        return this.http.post(FEEDBACK_API_URL, data, options)
+        return this.http.post(FEEDBACK_API_URL + '?key=' + FEEDBACK_API_TOKEN, data, options)
     }
 }
