64bc6bd25f412116fb7ecf5dc383312f5bd0e829
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2017-06-14 18:12:21 +0200

    Implements navigation launching

--- src/components/direction-button/direction-button.ts
+++ src/components/direction-button/direction-button.ts
@@ -1,6 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { LaunchNavigator } from '@ionic-native/launch-navigator';
+import { Platform } from 'ionic-angular';
 import * as Raven from 'raven-js';
+
+import { LocationService } from '../../services/location/location';
+
+declare var launchnavigator: any;
 
 @Component({
     selector: 'direction-button',
@@ -11,20 +16,60 @@ export class DirectionButton {
 
     @Input() coordinates: any;
 
-    constructor(private launchNavigator: LaunchNavigator) {}
+    private navigationApp: any;
 
-    startNavigation() {
-        this.launchNavigator.navigate(
-            [
-                this.coordinates.latitude,
-                this.coordinates.longitude
-            ],
-            {
-                transportMode: 'walking',
-                // appSelectionDialogHeader: 'Sélectionnez une application de navigation',
-                // appSelectionCancelButton: 'Annuler'
+    constructor(
+        private launchNavigator: LaunchNavigator,
+        private platform: Platform,
+        private locationService: LocationService
+    ) {
+        launchnavigator.isAppAvailable(launchnavigator.APP.GOOGLE_MAPS, isAvailable => {
+            // priority to Google Maps app
+            if (isAvailable){
+                this.navigationApp = launchnavigator.APP.GOOGLE_MAPS;
+            } else {
+                // else, on Android, open native gep selector application
+                if (platform.is('android')) {
+                    this.navigationApp = launchnavigator.APP.GEO;
+                }
+                // else, open app selector
+                else {
+                    this.navigationApp = launchnavigator.APP.USER_SELECT;
+                }
             }
-        ).catch(error => {
+        });
+    }
+
+    /**
+     * Start navigation to `coordinates`
+     */
+    navigate() {
+        this.launchNavigation(null, [
+            this.coordinates.latitude,
+            this.coordinates.longitude
+        ]);
+    }
+
+    /**
+     * Launch navigation app using givent coordinates.
+     *
+     * @param {number[]} from
+     * @param {number[]} to
+     */
+    launchNavigation(from: number[], to: number[]) {
+        this.launchNavigator.navigate(
+            to,
+            {
+                start: from,
+                app: this.navigationApp,
+                transportMode: 'walking',
+                // launchMode: 'turn-by-turn',
+                appSelectionDialogHeader: 'Sélectionnez une application de navigation',
+                appSelectionCancelButton: 'Annuler'
+            }
+        )
+        .catch(error => {
+            // TODO: check if errors a relevant
             Raven.captureException(new Error(error));
         });
     }
