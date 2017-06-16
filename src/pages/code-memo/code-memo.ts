16b2eca1d7cffc1874c048e50be8a332219c7ee6
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2017-06-16 14:30:04 +0200

    Implements camera preview WIP

--- src/pages/code-memo/code-memo.ts
+++ src/pages/code-memo/code-memo.ts
@@ -1,5 +1,5 @@
 import { Component, AfterViewInit } from '@angular/core';
-import { ViewController } from 'ionic-angular';
+import { ViewController, Platform } from 'ionic-angular';
 import * as Raven from 'raven-js';
 
 import { CameraPreview } from '@ionic-native/camera-preview';
@@ -18,7 +18,8 @@ export class CodeMemo implements AfterViewInit {
 
     constructor(
         private viewCtrl: ViewController,
-        private cameraPreview: CameraPreview
+        private cameraPreview: CameraPreview,
+        private platform: Platform
     ) {
         // STUMB
         this.code = "____";
@@ -29,14 +30,14 @@ export class CodeMemo implements AfterViewInit {
     }
 
     startPreview() {
-        let bound = document.getElementsByClassName('lock-code')[0].getBoundingClientRect();
+        let bounds = document.getElementsByClassName('lock-code')[0].getBoundingClientRect();
 
         this.isPreviewRunning = true
         this.cameraPreview.startCamera({
-            x: bound.left,
-            y: bound.top,
-            height: bound.height,
-            width: bound.width,
+            x: bounds.left,
+            y: bounds.top,
+            height: bounds.height,
+            width: bounds.width,
             camera: this.cameraPreview.CAMERA_DIRECTION.BACK,
             tapPhoto: false,
             // tapFocus: true
@@ -58,11 +59,12 @@ export class CodeMemo implements AfterViewInit {
     }
 
     runOCR() {
-        let bound = document.getElementsByClassName('lock-code')[0].getBoundingClientRect();
+        let bounds = document.getElementsByClassName('lock-code')[0].getBoundingClientRect();
 
         this.cameraPreview.takePicture({
-            // width: bound.width,
-            // height: bound.height
+            width: this.platform.width(),
+            height: this.platform.height(),
+            quality: 50
         }).then(base64PictureData => {
             this.stopPreview();
             this.code = "load";
@@ -71,16 +73,18 @@ export class CodeMemo implements AfterViewInit {
         }).then(croppedBase64PictureData => {
             console.log(croppedBase64PictureData)
 
-            this.ocradImageSrc = croppedBase64PictureData;
+            this.ocradImageSrc = croppedBase64PictureData
 
-            (<any>window).OCRAD(document.getElementById('ocrad-image'), {
-                // numeric: true
-            }, text => {
-                this.code = text.substring(0, 4);
-            }, error => {
-                this.code = "error"
-                console.log(error);
-            });
+            let imageElement = new Image(bounds.height, bounds.width);
+            imageElement.src = croppedBase64PictureData;
+
+            imageElement.onload = () => {
+                (<any>window).OCRAD(imageElement, {
+                    // numeric: true
+                }, text => {
+                    this.code = text.substring(0, 4);
+                });
+            };
         });
     }
 
@@ -88,19 +92,40 @@ export class CodeMemo implements AfterViewInit {
         let image = new Image();
         image.src = 'data:image/jpeg;base64,' + base64;
 
+        let bounds = document.getElementsByClassName('lock-code')[0].getBoundingClientRect();
+
+        console.log(bounds)
+
         let canvas = document.createElement('canvas');
+        canvas.height = bounds.height;
+        canvas.width = bounds.width;
+
         let ctx = canvas.getContext('2d');
 
-        let bound = document.getElementsByClassName('lock-code')[0].getBoundingClientRect();
-
         return new Promise((resolve, reject) => {
-            image.onload = function() {
+            image.onload = () => {
+                console.log('Source image:');
+                console.log('w: ' + image.width);
+                console.log('h: ' + image.height);
+
+                let c = image.height / this.platform.height();
+
+                let offsetTop = bounds.top * c;
+                let offsetLeft = bounds.left * c;
+                let sourceHeight = bounds.height * c;
+
+                console.log('Computed:');
+                console.log('w: ' + image.width);
+                console.log('h: ' + sourceHeight);
+                console.log('top: ' + offsetTop);
+                console.log('left: ' + offsetLeft)
+
                 ctx.drawImage(
                     image,
+                    0, offsetTop,
+                    image.width, sourceHeight,
                     0, 0,
-                    image.width, image.height,
-                    0, 0,
-                    bound.width, bound.height
+                    bounds.width, bounds.height
                 );
 
                 resolve(canvas.toDataURL("image/jpeg", 0.5));
@@ -109,6 +134,7 @@ export class CodeMemo implements AfterViewInit {
     }
 
     close() {
+        this.stopPreview();
         this.viewCtrl.dismiss();
     }
 }
