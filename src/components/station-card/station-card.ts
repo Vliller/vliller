257cc505757807f9740c7b06d6ead568114cc351
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2017-06-26 11:41:59 +0200

    refactor(formatedDistance): moves the formatedDistance() method as an accessoir of VlillerStation model

--- src/components/station-card/station-card.ts
+++ src/components/station-card/station-card.ts
@@ -9,39 +9,6 @@ import { VlilleStation } from '../../services/vlille/vlille';
 })
 
 export class StationCard {
-
-    @Input()
-    public station: VlilleStation;
-
-    @Input()
-    public isLoaded: boolean = true;
-
-    constructor() {}
-
-    /**
-     * Return a string format in meters or kilometers.
-     *
-     * @return {string}
-     */
-    public formatedDistance() {
-        let distanceInMeter = (<any>this.station).distance,
-            distanceString = 'à ';
-
-        // to avoid blinking
-        if (!distanceInMeter) {
-            return '';
-        }
-
-        // meters
-        if (distanceInMeter < 1000) {
-            distanceString += Math.round(distanceInMeter) + 'm';
-        }
-
-        // kilometers
-        else {
-            distanceString += (Math.round(distanceInMeter / 100) / 10) + 'km';
-        }
-
-        return distanceString;
-    };
+    @Input() station: VlilleStation;
+    @Input() isLoaded: boolean = true;
 }
