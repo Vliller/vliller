1780287a2173e9341bf7582e54a2318a0d01b8ce
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2017-07-12 16:25:31 +0200

    refactor(location): removes deadcode

--- src/services/location/location.ts
+++ src/services/location/location.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { Platform } from 'ionic-angular';
 import { Geolocation, Geoposition } from '@ionic-native/geolocation';
 import { Diagnostic } from '@ionic-native/diagnostic';
@@ -10,8 +9,6 @@ import { MapPosition } from '../../components/map/map-position';
 
 @Injectable()
 export class LocationService {
-    // replay the last value to each new subscriber
-    private currentPositionSubject = new ReplaySubject<MapPosition>(1);
 
     constructor(private platform: Platform) {}
 
@@ -47,20 +44,6 @@ export class LocationService {
     }
 
     /**
-     * Updates the current Observable position and resolved a promise with the new position.
-     * @return {Promise<MapPosition>}
-     */
-    public updateCurrentPosition(): Promise<MapPosition> {
-        return this.getCurrentPosition()
-            .then(position => {
-                // Update stream
-                this.currentPositionSubject.next(position);
-
-                return position;
-            });
-    }
-
-    /**
      * Checks if the location is enabled (promise resolved) or disabled (promise rejected)
      * @return {Promise<any>}
      */
@@ -76,13 +59,5 @@ export class LocationService {
                 return locationAccuracy.request(locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY);
             });
         });
-    }
-
-    /**
-     * Returns an observable on the user position
-     * @return {Observable<MapPosition>}
-     */
-    public asObservable(): Observable<MapPosition> {
-        return this.currentPositionSubject.asObservable();
     }
 }
