f0e288b3cabc1f9cc348fe9f68aeae295aa2b769
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2017-09-15 18:06:29 +0200

    Fix indentation on MapIcon object

--- src/components/map/map-icon.ts
+++ src/components/map/map-icon.ts
@@ -2,59 +2,59 @@
  * All icons are in retina size
  */
 export const MapIcon = {
-    // Normal state
-    NORMAL: {
-        url: 'www/assets/img/vliller-marker-normal@2x.png',
-        size: {
-            width: 34,
-            height: 40
-        }
-    },
-    NORMAL_ACTIVE: {
-        url: 'www/assets/img/vliller-marker-normal-active@2x.png',
-        size: {
-            width: 62,
-            height: 72
-        }
-    },
-    NORMAL_SMALL: {
-        url: 'www/assets/img/vliller-marker-normal-small@2x.png',
-        size: {
-            width: 10,
-            height: 10
-        }
-    },
-
-    // Station unavaible
-    UNAVAIBLE: {
-        url: 'www/assets/img/vliller-marker-unavaible@2x.png',
-        size: {
-            width: 34,
-            height: 40
-        }
-    },
-    UNAVAIBLE_ACTIVE: {
-        url: 'www/assets/img/vliller-marker-unavaible-active@2x.png',
-        size: {
-            width: 62,
-            height: 72
-        }
-    },
-    UNAVAIBLE_SMALL: {
-        url: 'www/assets/img/vliller-marker-unavaible-small@2x.png',
-        size: {
-            width: 10,
-            height: 10
-        }
-    },
-
-    // User position
-    USER: {
-        url: 'www/assets/img/vliller-marker-user@2x.png',
-        size: {
-            width: 18,
-            height: 30
-        },
-        anchor: [9, 21]
+  // Normal state
+  NORMAL: {
+    url: 'www/assets/img/vliller-marker-normal@2x.png',
+    size: {
+      width: 34,
+      height: 40
     }
+  },
+  NORMAL_ACTIVE: {
+    url: 'www/assets/img/vliller-marker-normal-active@2x.png',
+    size: {
+      width: 62,
+      height: 72
+    }
+  },
+  NORMAL_SMALL: {
+    url: 'www/assets/img/vliller-marker-normal-small@2x.png',
+    size: {
+      width: 10,
+      height: 10
+    }
+  },
+
+  // Station unavaible
+  UNAVAIBLE: {
+    url: 'www/assets/img/vliller-marker-unavaible@2x.png',
+    size: {
+      width: 34,
+      height: 40
+    }
+  },
+  UNAVAIBLE_ACTIVE: {
+    url: 'www/assets/img/vliller-marker-unavaible-active@2x.png',
+    size: {
+      width: 62,
+      height: 72
+    }
+  },
+  UNAVAIBLE_SMALL: {
+    url: 'www/assets/img/vliller-marker-unavaible-small@2x.png',
+    size: {
+      width: 10,
+      height: 10
+    }
+  },
+
+  // User position
+  USER: {
+    url: 'www/assets/img/vliller-marker-user@2x.png',
+    size: {
+      width: 18,
+      height: 30
+    },
+    anchor: [9, 21]
+  }
 };
