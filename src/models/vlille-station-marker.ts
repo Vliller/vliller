0aed7e19063a390afbc03560800b378314e7bdd2
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2017-11-05 22:57:01 +0100

    Implements updateIcon() on station marker

--- src/models/vlille-station-marker.ts
+++ src/models/vlille-station-marker.ts
@@ -1,4 +1,5 @@
 import { MapMarker } from '../components/map/map-marker';
+import { MapIcon, DynamicMapIcon } from '../components/map/map-icon';
 import { VlilleStation, VlilleStationStatus } from './vlille-station';
 
 export class VlilleStationMarker extends MapMarker {
@@ -18,8 +19,34 @@ export class VlilleStationMarker extends MapMarker {
     throw new Error("Method not implemented.");
   }
 
-  updateIcon() {
-    throw new Error("Method not implemented.");
+  updateIcon(isMapUnzoom: boolean, isActive: boolean = false) {
+    // active marker
+    if (isActive) {
+      if (this.isAvailable()) {
+        this.marker.setIcon(MapIcon.NORMAL_ACTIVE);
+      } else {
+        this.marker.setIcon(MapIcon.UNAVAILABLE_ACTIVE);
+      }
+
+      return;
+    }
+
+    // unactive marker
+    if (isMapUnzoom) {
+      if (this.isAvailable()) {
+          this.marker.setIcon(MapIcon.NORMAL_SMALL);
+      } else {
+          this.marker.setIcon(MapIcon.UNAVAILABLE_SMALL);
+      }
+    } else {
+      if (this.isAvailable()) {
+          this.marker.setIcon(DynamicMapIcon.getIcon(this.station.usageInPercent));
+      } else {
+          this.marker.setIcon(MapIcon.UNAVAILABLE);
+      }
+    }
+
+
   }
 
   isAvailable(): boolean {
