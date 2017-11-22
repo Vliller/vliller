88d88b9ea7956e755079bdaffc8e329d947e2fb3
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2017-11-22 20:12:04 +0100

    Fix interface empty return value typing

--- src/models/map-marker-interface.ts
+++ src/models/map-marker-interface.ts
@@ -3,9 +3,9 @@ import { Observable } from 'rxjs/Observable';
 
 export interface MapMarkerInterface {
   onClick(): Observable<ILatLng>;
-  isEqual(marker: MapMarkerInterface);
-  setIcon(icon: MarkerIcon);
-  setMarker(marker: Marker);
+  isEqual(marker: MapMarkerInterface): void;
+  setIcon(icon: MarkerIcon): void;
+  setMarker(marker: Marker): void;
   getMarker(): Marker;
-  removeMarker();
+  removeMarker(): void;
 }
